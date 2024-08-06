6d97fa7d80b58e1ebde97ab9202bfc9957e7bd05
Rafael Almeida <dealmeida.silva@hotmail.com>
2024-08-06 14:19:08 -0300

    Commit do exercicio

--- mysite/tests/models/test_post.py
+++ mysite/tests/models/test_post.py
@@ -2,10 +2,10 @@ import pytest
 
 from blog.factories import PostFactory
 
-
 @pytest.fixture
 def post_published():
     return PostFactory(title='pytest with factory')
+
 
 
 @pytest.mark.django_db
